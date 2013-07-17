3ca6ab5b83cff479e8e2b3d55a144b4d45c7f9ba
Jon <jdavis@K0376.local>
2013-07-16 21:20:34 -0700

    Migrated to text widget, put in counters & err check

--- app.js
+++ app.js
@@ -5,48 +5,76 @@ var geck = new Geckoboard({api_key: config.GeckoAPIKey});
 var gText = geck.text(config.GeckoWidget);
 
 // Prep var
-var items = [];
+var items;
+var items2;
 
-// Make a request to roomwarden API
-require('request').get({ 
-	uri:config.RoomwardenURL,
-	headers:{'Accept':'application/json','Content-type':'application/json'},
-	},function (err,res,body){
-		var out = JSON.parse(body);
-		console.log("config.RoomwardenURL = " + res.statusCode);
-		buildItems(out);
-	});
 
+function room(){
+    // Make a request to roomwarden API
+    console.log('This is Red Five; I\'m going in! '+getDateTime());
+
+    require('request').get({ 
+    	uri:config.RoomwardenURL,
+    	headers:{'Accept':'application/json','Content-type':'application/json'},
+    	},function (err,res,body){
+    		var out = JSON.parse(body);
+    		console.log('config.RoomwardenURL = ' + res.statusCode);
+            if(res.statusCode == 200){
+                //It worked, move on
+                buildItems(out);
+            }else{
+                // Didn't work, wait and try again.
+                setTimeout(room,5000);
+            }
+    		
+    	});
+}
 
 function buildItems(out){
-// Build the variable/JSON for output
+    // Build the variable/JSON for output
+    items = '<font size="-1">';
 	out['item'].forEach(function(line) {
-		console.log(line['text']);
+		//console.log(line['text']);
         if(line['value'] == 'open'){var color = "#00FF00";}else{var color = "#FF0000";}
-        items[items.length] = {
-            "label": {
-                "name": line['value'],
-                "color": color
-            },
-            "title": {
-                "text": line['text'],
-                "highlight": true,
-            },
-            "description": "Since "+line['timeago']
-        };
-
-
+        items += '<div style="background-color:'+color+'; color: #FFFFFF;">'+line['value']+' -- '+line['text']+'</div>';
+        items += '<div> Since '+line['timeago']+'</div><br>'+"\n";
 	});
-
+    items += '<br>'+getDateTime()+'</font>';
+    items2 = {"text" : items};
 	sendIt();
 }
 
 function sendIt(){
-// Send the data to Geckoboard
-console.log(items);
-	list.send(items, function(err, response){
-		console.log(response);
+    // Send the data to Geckoboard
+    //console.log(items2);
+	gText.send(items2, function(err, res2){
+        if(res2['success'] == true){
+            // We're done here! Wait for 45 seconds
+            console.log('sendIt - OK');
+            setTimeout(room,45000);
+        }else{
+            // Didn't work, wait and try again
+            console.log(res2);
+            setTimeout(sendIt,5000);
+        }
 	});
-
 }
 
+function getDateTime() {
+    var date = new Date();
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+    var sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+    var day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+    return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec;
+}
+
+//EXECUTE!
+room();
